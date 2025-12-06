0fe40d508487b2e22e8bdfa753196b6a6342c70c
sodhims <sodhi@uri.edu>
2025-12-06 00:40:55 -0500

    fixed index

--- obj/Debug/net9.0/dfd2wasm.AssemblyInfo.cs
+++ obj/Debug/net9.0/dfd2wasm.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dfd2wasm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6830848261ae73903e657f05638b1d4bc380e3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("dfd2wasm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dfd2wasm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
