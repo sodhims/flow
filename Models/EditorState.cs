04ab0786330fd05ec6aeab938d4602357360dc58
sodhims <sodhi@uri.edu>
2025-12-06 14:35:36 -0500

    Update site from publish

--- Models/EditorState.cs
+++
@@ -1,9 +0,0 @@
-namespace dfd2wasm.Models
-{
-    public class EditorState
-    {
-        public List<Node> Nodes { get; set; } = new();
-        public List<Edge> Edges { get; set; } = new();
-        public List<EdgeLabel> EdgeLabels { get; set; } = new();
-    }
-}
